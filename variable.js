cfcfcc0310883731b9d9fdf41c89f53fc3142d1c
Bibek Bhandari <bhandaribibek731@gmail.com>
2023-02-26 20:21:32 +0545

    variable

---
+++ variable.js
@@ -0,0 +1,6 @@
+let a=67 //let is used to define varibale in js
+console.log(a)
+
+//example to show that in js,js variable can be changed during the execution of a program.
+a="bibek"
+console.log(a)
