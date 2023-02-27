1d0b734c2e87b7714bc567a9d02ee35132e29d9c
Bibek Bhandari <bhandaribibek731@gmail.com>
2023-02-27 17:46:49 +0545

    operation

---
+++ operator.js
@@ -0,0 +1,47 @@
+//operators in js
+console.log("operation in js")
+let a = 10;
+let b = 4;
+console.log("a+b=", a + b)
+console.log("a-b=", a - b)
+console.log("a/b=", a / b)
+console.log("a*b=", a * b)
+console.log("a**b=", a ** b)
+console.log("a%b=", a % b)
+
+console.log("++a=", ++a)
+console.log("a++=", a++)
+console.log("--a=", --a)
+console.log("a--=", a--)
+console.log("a=", a)
+console.log("a--=", a--)
+
+//assignment operators
+let ass = 1;
+ass += 5 // same as ass=ass+5
+console.log("ass is now=", ass)
+ass -= 5 // same as ass=ass-5
+console.log("ass is now=", ass)
+ass *= 5 // same as ass=ass*5
+console.log("ass is now=", ass)
+ass /= 5 //same as ass=ass/5
+console.log("ass is now=", ass)
+
+// cpmparison operator
+let ab = 6;
+let cd = "6";
+console.log("ab==cd is",ab==cd)
+console.log("ab!=cd is",ab!=cd)
+console.log("ab===cd is",ab===cd) // === equal type and equal value then show true natra false.
+console.log("ab!==cd is",ab!==cd)
+console.log("ab>cd is",ab>cd)
+
+//logical operation
+let x=5;
+let y=6;
+console.log("x<y&&x==5",x<y&&x==5)
+console.log("x>y||x==5",x>y&&x==5)
+console.log("!false",!false)
+console.log("!true",!true)
+
+
