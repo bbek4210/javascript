af02a58852713eae501f2bdbbef42a740b62c931
Bibek Bhandari <bhandaribibek731@gmail.com>
2023-02-28 23:21:17 +0545

    switch

--- switch.js
+++ switch.js
@@ -1,18 +1,17 @@
-let a=2;
-switch(a){
-    case 1:
-        a='one';
-        break;
-        case 2 :
-            a='two';
-            break;
-            default:
-                a='not found';
-                break;
+let a = 2;
+switch (a) {
+  case 1:
+    a = "one";
+    break;
+  case 2:
+    a = "two";
+    break;
+  default:
+    a = "not found";
+    break;
 }
 console.log(`The value is ${a}`);
 
 //ternary operator
 
-
-console.log("you can",(a>18?"not drive":"drive"))
+console.log("you can", a > 18 ? "not drive" : "drive");
