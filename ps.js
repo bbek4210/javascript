323a1f822576279535a934b579bb1e960a940506
Bibek Bhandari <bhandaribibek731@gmail.com>
2023-02-28 23:20:31 +0545

    switch

---
+++ ps.js
@@ -0,0 +1,26 @@
+//use logical operator to find whether the age of a person lies between 10 and 20
+let age = prompt("whats your age");
+if (age > 10 && age < 20) {
+  console.log("your age lies btween 10 and 20");
+} else {
+  console.log("your age doesnt lies btween 10 and 20");
+}
+
+//use of switch
+let num = prompt("whats your num");
+switch (num) {
+  case 12:
+    console.log("your number is 12");
+    break;
+    case13: console.log("your number is 13");
+    break;
+    case14: console.log("your number is 14");
+    break;
+  default:
+    console.log("your number is not defined");
+    break;
+}
+
+//wap is js to find whether a number is divisible bt 2 and 3
+
+let num=prompt("what is your num")
