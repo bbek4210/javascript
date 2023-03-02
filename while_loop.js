bcb83fd31aa1cd282fd49ee5b811503398c98f26
Bibek Bhandari <bhandaribibek731@gmail.com>
2023-03-02 19:36:22 +0545

    while_dowhile

---
+++ while_loop.js
@@ -0,0 +1,19 @@
+// the while loop
+
+let n=prompt("whats the value of n")
+n=Number.parseInt(n)
+let i=1
+while(i<n){
+    console.log(i)
+    i++;
+}
+
+
+// do-while loop
+let a=prompt("whats the value of a")
+a=Number.parseInt(a)
+let y=10
+do{
+    console.log(y)
+    y++
+}while(i<a)
