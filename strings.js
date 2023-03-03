aaafd83dd7d7bb251843f372c7f53d4c95a984e8
Bibek Bhandari <bhandaribibek731@gmail.com>
2023-03-03 20:03:23 +0545

    strings

---
+++ strings.js
@@ -0,0 +1,26 @@
+let name="bibekk"
+console.log(name)
+console.log(name.length)
+console.log(name[0])
+console.log(name[4])
+
+//template literals = backtick ` `
+
+let friend=`bibika`
+console.log(friend)
+
+let boy1 ="pramod"
+let boy2="nikhil"
+//nikhil is a friend of pramod
+let sentence=`${boy2} is a friend of ${boy1}`
+console.log(sentence)
+
+//escape sequence charcaters
+let fruit='bana\'na'
+console.log(fruit)
+
+let laptop='dell\nlenevo'
+console.log(laptop)
+
+let mobile='oppo\tsamsung'
+console.log(mobile)
