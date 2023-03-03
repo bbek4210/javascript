707c72b2bbb1d6633ca27efedce112bf8f4c8b6f
Bibek Bhandari <bhandaribibek731@gmail.com>
2023-03-03 22:56:41 +0545

    string method

---
+++ strings_methods.js
@@ -0,0 +1,28 @@
+let name="Jacquilene"
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.slice(2,6)) // prints from 2 to 6 , 6 not included
+console.log(name.slice(2)) // from 2 to end
+console.log(name.replace("Jacq","Racq"))
+
+
+let friend='    salman   '
+console.log(friend)
+console.log(friend.trim())
+
+
+let fruit="banana"
+let fruit1="mango"
+let fruit2="orange"
+console.log(fruit+fruit1+fruit2)
+
+
+//use a for loop to print a string
+
+let x="hello"
+let i=0
+for (i=0;i<x.length;i++){
+    
+   console.log(x[i])
+}
