504b92df4be9e9b611f4e14c2269a1e801795a0e
Bibek Bhandari <bhandaribibek731@gmail.com>
2023-03-05 22:23:36 +0545

    arrays

---
+++ arrays.js
@@ -0,0 +1,8 @@
+let a=[91,45,87,46]
+console.log(a)
+console.log(a[1])
+console.log(a.length)
+a[4]=89//adding a new value to the array
+console.log(a)
+a[0]=98//changing the value of an array
+console.log(a[0])
