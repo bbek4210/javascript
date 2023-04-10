5a8a086d9161ac64487846d2d6cc15b75f9fa01c
Bibek Bhandari <bhandaribibek731@gmail.com>
2023-04-10 07:53:01 +0545

    calc

---
+++ calculator.js
@@ -0,0 +1,14 @@
+const operand1 = document.getElementById('txtoperand1');
+const operand2 = document.getElementById('txtoperand2');
+const resultField = document.getElementById('txtResult');
+
+
+
+document.getElementById('btnAdd').addEventListener('click', function() {
+    const result = Number(operand1.value) + Number(operand2.value);
+    resultField.value = result;
+  });
+  document.getElementById('btnSubtract').addEventListener('click', function() {
+    const result = Number(operand1.value) - Number(operand2.value);
+    resultField.value = result;
+  });
