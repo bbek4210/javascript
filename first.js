5234e14ae4474c8d6b4cbd6cebb8ed000b8352d6
Bibek Bhandari <bhandaribibek731@gmail.com>
2023-05-16 10:29:43 +0545

    nd

--- first.js
+++ first.js
@@ -1,30 +1,51 @@
-console.log("hello bibek")
-function addThreeNumbers(a, b, c){
-    return a+b+c;
-}
-    let c = addThreeNumbers(1,2,3)
-    console.log(c)
+// // console.log("hello bibek")
+// // function addThreeNumbers(a, b, c){
+// //     return a+b+c;
+// // }
+// //     let c = addThreeNumbers(1,2,3)
+// //     console.log(c)
 
 
-    let random = Math.floor(Math.random() * 101);
-console.log(random)
-let chances = 0;
-let input;
-while (input != random){
-  input = Number.parseInt(prompt("Enter a Number between 0 to 100"));
+// //     let random = Math.floor(Math.random() * 101);
+// // console.log(random)
+// // let chances = 0;
+// // let input;
+// // while (input != random){
+// //   input = Number.parseInt(prompt("Enter a Number between 0 to 100"));
   
-  if(input != random){
-  console.log("Wrong! Try again.")}
+// //   if(input != random){
+// //   console.log("Wrong! Try again.")}
   
-  if (random>input){
-    console.log("The Number is bigger than " + input)
-  }
-  else if (random<input) {
-    console.log("The Number is smaller than " + input)
-  }
+// //   if (random>input){
+// //     console.log("The Number is bigger than " + input)
+// //   }
+// //   else if (random<input) {
+// //     console.log("The Number is smaller than " + input)
+// //   }
   
-  chances += 1;
-}
-console.log("Congrats Champ!")
-console.log(`You scored ${100-chances} points.`)
+// //   chances += 1;
+// // }
+// // console.log("Congrats Champ!")
+// // console.log(`You scored ${100-chances} points.`)
 
+
+// // var store;
+// // var myName=prompt("enter your name");
+// // for (i=myName.length -1; i<myName.length; i--) {
+// // let a=store.push(myName[i]);
+// // }
+// // console.log("a")
+
+
+
+// // let inputString = prompt("Enter a word to reverse:");
+// // let reversedString = "";
+
+// // // Loop through the input string backwards and add each character to the reversed string
+// // for (let i = inputString.length - 1; i >= 0; i--) {
+// //   reversedString += inputString[i];
+// // }
+
+// // console.log(`The reversed word is: ${reversedString}`);
+
+// console.log(new Date().toLocaleString());
